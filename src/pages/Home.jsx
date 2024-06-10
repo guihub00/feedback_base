756cce6d28eaecff80c008219faa220de19e64f0
guilherme rocha <guilhermepereira0720@gmail.com>
2024-06-10 02:14:49 +0000

    commit 5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import { ButtonContainer, CardContainer, IconContainer } from "./Home.styles";
+import iconStarImg from "../assets/icon-star.svg"
+
 export function Home() {
   return (
-    <div>Home</div>
+    <CardContainer>
+      <IconContainer>
+        <img src={iconStarImg} alt="" />
+      </IconContainer>
+
+      <h1>Como foi o atendimento?</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod vitae soluta labore nostrum eaque quo minus iure libero, necessitatibus quam. Molestias quisquam harum pariatur quam quasi ad expedita deleniti perspiciatis.</p>
+
+      <ButtonContainer>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </ButtonContainer>
+
+      <button>Enviar</button>
+    </CardContainer>
   )
 }

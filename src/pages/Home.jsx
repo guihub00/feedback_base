fda64ffaa5f4038e56cb1f254736992263c8ec66
guilherme rocha <guilhermepereira0720@gmail.com>
2024-06-10 04:05:22 +0000

    acabou

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,24 @@ import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/ilustration-thank-you.svg"
 
 export function Home() {
-  let aparecerResultado = true
+  const [mostrarResultado, setMostrarResultado] = useState(false)
+  const [feedbackNote, setFeedbackNote] = useState(0)
+
+  function handleFeedbackButtonClick(event) {
+    const feedback = Number(event.target.innerText)
+
+    setFeedbackNote(feedback)
+
+  }
+
+  function handleSubmit() {
+    if (feedbackNote === 0) return
+
+    setMostrarResultado(true)
+  }
 
   return (
-    aparecerResultado === false ? 
+    mostrarResultado === false ? 
     <CardContainer>
     <IconContainer>
       <img src={iconStarImg} alt="" />
@@ -16,14 +30,14 @@ export function Home() {
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod vitae soluta labore nostrum eaque quo minus iure libero, necessitatibus quam. Molestias quisquam harum pariatur quam quasi ad expedita deleniti perspiciatis.</p>
 
     <ButtonContainer>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
+      <button onClick={handleFeedbackButtonClick}>1</button>
+      <button  onClick={handleFeedbackButtonClick}>2</button>
+      <button  onClick={handleFeedbackButtonClick}>3</button>
+      <button  onClick={handleFeedbackButtonClick}>4</button>
+      <button  onClick={handleFeedbackButtonClick}>5</button>
     </ButtonContainer>
 
-    <button>Enviar</button>
+    <button onClick={handleSubmit}>Enviar</button>
 
     </CardContainer>
      
@@ -32,7 +46,7 @@ export function Home() {
     <img src={thankYouImg} alt="" />
 
      <ResultContainer>
-        <p>Você selecinou 4 de 5</p>
+        <p>Você selecinou {feedbackNote} de 5</p>
       </ResultContainer>
 
 
